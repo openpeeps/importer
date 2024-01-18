d65c81ed140a6e8eae5c27bd196c1ff0c2f0faf9
George Lemon <georgelemon@protonmail.com>
2024-01-18 18:06:09 +0200

    update
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/importer.nim
+++ src/importer.nim
@@ -1,6 +1,6 @@
 # A generic file importer that can be used
-# by hand-crafted parsers and interpreters to
-# build high performance import module systems
+# by hand-crafted parsers & interpreters to
+# build high performance import module systems.
 #
 # (c) 2023 George Lemon | MIT License
 #          Made by Humans from OpenPeeps
@@ -19,34 +19,41 @@ type
     info*: FileInfo
 
   ImportQueue* = seq[string]
+    ## A queue sequence
   ImportResolved = TableRef[string, ImportFile]
-  
+
   ImportSourcePolicy* = enum
-    localSourcePolicy
-    remoteSourcePolicy
-    anySourcePolicy
+    ## Defines Importer Policy. By default, only
+    ## local files can be imported.
+    spLocal
+    spRemote
+    spAny
 
   ImportErrorMessage* = enum
+    ## Some error codes to be used at parser-level
     importCircularError
     importDuplicateFile
     importNotFound
 
-  ImportPolicy* = ref object # todo
+  ImportPolicy* = ref object
     case sourcePolicy: ImportSourcePolicy
-    of remoteSourcePolicy, anySourcePolicy:
+    of spRemote, spAny:
       client: HttpClient
       secured: bool = true
       whitelist: seq[Uri]
     else: discard
     extensions: seq[string]
+      # a seq of allowed file extensions
+
+  FailedImportTuple* = tuple[reason: ImportErrorMessage, fpath: string]
 
   Import*[T] = ref object
     handle*: T
     master: Master
-    resolved: ImportResolved 
+    resolved: ImportResolved
     mainFilePath, mainDirPath, mainId: string
-    fails: seq[tuple[reason: ImportErrorMessage, fpath: string]]
-    failed: bool
+    fails: seq[FailedImportTuple]
+      # a seq of failed imports containing the file path and `ImportErrorMessage` 
 
   ImportHandle*[T] = proc(imp: Import[T], file: ImportFile, ticket: ptr TicketLock): seq[string] {.gcsafe, nimcall.}
   ImportError* = object of CatchableError
@@ -72,8 +79,8 @@ proc newImport*[T](path: string, basepath = "", baseIsMain = false): Import[T] =
   raise newException(ImportError, "Main file not found\n" & path)
 
 proc error[T](i: Import[T], reason: ImportErrorMessage, fpath: string) =
+  # Used to mark failed imports in spawned tasks
   add i.fails, (reason, fpath)
-  if not i.failed: i.failed = true
 
 proc resolver[T](i: Locker[Import[T]], m: MasterHandle, fpath: string,
       parseHandle: ptr ImportHandle[T], ticket: ptr TicketLock) {.gcsafe.} =
@@ -84,20 +91,16 @@ proc resolver[T](i: Locker[Import[T]], m: MasterHandle, fpath: string,
     if likely(fpath.fileExists()):
       if likely(fpath != imp.mainFilePath):
         if not imp.resolved.hasKey(fpath):
-          # parse a new file
+          # invoke a new parser instance for given `fpath`
           var importFile = ImportFile(path: fpath, source: readFile(fpath))
           imp.resolved[fpath] = importFile
-          imp.resolved[fpath].cached = true
+          # imp.resolved[fpath].cached = true # is this useful?
           let fpaths: seq[string] = parseHandle[](imp, importFile, ticket)
           if fpaths.len > 0:
-            for otherpath in fpaths:
-              imp.master.spawn resolver(i, imp.master.getHandle, otherpath, parseHandle, ticket)
-        else:
-          # reuse imported file
-          discard parseHandle[](imp, imp.resolved[fpath], ticket)
+            for f in fpaths:
+              imp.master.spawn resolver(i, imp.master.getHandle, f, parseHandle, ticket)
       else: imp.error(importCircularError, fpath)
-    else:
-      imp.error(importNotFound, fpath)
+    else: imp.error(importNotFound, fpath)
 
 proc imports*[T](imp: Import[T], files: seq[string], parseHandle: ImportHandle[T]) =
   var isolateImp = initLocker(imp)
@@ -106,12 +109,11 @@ proc imports*[T](imp: Import[T], files: seq[string], parseHandle: ImportHandle[T
     for fpath in files:
       imp.master.spawn resolver(isolateImp, imp.master.getHandle, fpath, addr(parseHandle), addr(ticket))
 
-proc isCached*(importFile: ImportFile): bool =
-  ## Check if `ImportFile` is cached
-  importFile.cached
+# proc isCached*(f: ImportFile): bool = f.cached
+proc getImportPath*(f: ImportFile): string = f.path
 
-proc getImportPath*(importFile: ImportFile): string =
-  ## Get absolute path of `ImportFile`
-  importFile.path
+proc hasFailedImports*[T](imp: Import[T]): bool = imp.fails.len != 0
 
-proc hasError*[T](imp: Import[T]): bool = imp.failed
+iterator importErrors*[T](imp: Import[T]): FailedImportTuple =
+  for err in imp.fails:
+    yield err
